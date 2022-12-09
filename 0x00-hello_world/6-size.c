edc8d6abddcf431d1ad65e825db631e55e1f0f49
Kestarich <nyavor@outlook.com>
2022-12-08 18:22:20 -0800

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <studio.h>
+/**
+* main - Entry
+* Return: Always 0
+* /
+main(void)
+init main(void)
+{
+	printf(Size of a char:%ibytes(s)\n",sizof(char)):
+	printf(("Size of an int: %i byte(s)\n", sizeof(int));
+	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %i byte(s)\n", sizof(long long int));
+	printf("Size of a float: %i byte(s)\n", sizof(float));
+	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %i byte(s)\n", sizeof(float));
+	return (0);
+}
