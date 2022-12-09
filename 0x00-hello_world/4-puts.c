dfc49a4d4521981a8bf50591de6203f06b9add7c
Kestarich <nyavor@outlook.com>
2022-12-08 19:01:53 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
-  * main - Entry
-  * Description: 'hello world'
-  * 
-  * Return: Always 0 (success)
-  */
-int main(void) /*Function Declaration*/
+ * * main - Entry
+ * Description: 'hello world'
+ *
+ * Return: Always 0 (success)
+ */
+int main(void) /*Function Declearation*/
 {
-	puts("Programming is like building a multilingual puzzle);
+	puts("/"Programming is like building a multilingual puzzle);
 	return (0);
 }
