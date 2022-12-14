413d7b3d99e78b44bd4ad0bb52f46da7186c4d39
Kestarich <nyavor@outlook.com>
2022-12-13 18:41:11 -0800

    103

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,19 +6,14 @@
  *   *Return: 0 always
  */
 
-
 int main(void)
 
 {
 		int x = 1024, y, sum = 0;
-
 			for (y = 0; y < x; y++)
-
 					{
-
 								if ((y % 3 == 0) || (y % 5 == 0))
-											{
-															sum = sum + y;
+											{															sum = sum + y;
 																	}									}
 				printf("%d\n", sum);
 
