f8ea5f07743f0884ab28149c23ac49ccd996b878
Kestarich <nyavor@outlook.com>
2022-12-13 17:27:11 -0800

    3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,14 +3,13 @@
 
 
 /**
- *
  *  * _islower - function to check for lowercase character
  *
  *   *   * @c:  is the int that will use for the argument of the function
  *
  *    * Return: 0
  *
- *     */
+ */
 
 
 
