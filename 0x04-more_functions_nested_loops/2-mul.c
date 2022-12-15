e906bc8618840e8468d882578d59368e579267fc
Kestarich <nyavor@outlook.com>
2022-12-15 15:53:50 -0800

    2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+#include <stdio.h>
+
+
+
+/**
+ *  * mul -prints mul two numbers
+ *
+ *   *@a: print int a
+ *
+ *    *@b: print int b
+ *
+ *     * Return: Always 0.
+ */
+
+
+int mul(int a, int b)
+
+{
+
+	return (a * b);
+
+}
