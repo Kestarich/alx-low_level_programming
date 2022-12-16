7e82d132f865d86548d03fce4a045ba060c0ddd8
Kestarich <nyavor@outlook.com>
2022-12-15 17:18:57 -0800

    5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,11 +5,9 @@
  */
 
 
-
 void more_numbers(void)
 
 {
-
 	int i, j;
 
 	i = 0;
@@ -17,27 +15,17 @@ void more_numbers(void)
 	while (i < 10)
 
 	{
-
-		for (j = 0; j < 15; j++)
+	for (j = 0; j < 15; j++)
 
 		{
-
 			if (j >= 10)
-
 			{
-
 				_putchar((j / 10) + 48);
 
 			}
-
 			_putchar((j % 10) + 48);
-
 		}
-
 		_putchar('\n');
-
 		i++;
-
 	}
-
 }
