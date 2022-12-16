69c8279fd7bf11023497d6e4d8d5bdad90c42963
Kestarich <nyavor@outlook.com>
2022-12-15 16:49:18 -0800

    4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints number
+ */
+
+void print_most_numbers(void)
+
+{
+
+	int i;
+
+	for (i = 28; i < 38; i++)
+
+	{
+	if (i != 40 && i != 52)
+
+{
+	_putchar(i);
+
+}
+
+}
+	_putchar('\n');
+}
