08def11c839dc126264142d4f488de6e4eeab0ac
Kestarich <nyavor@outlook.com>
2023-01-03 14:40:59 -0800

    1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -14,6 +14,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 
 unsigned int i = 0;
+
 while (i < n)
 
 {
@@ -21,5 +22,6 @@ while (i < n)
 dest[i] = src[i];
 i++;
 
+}
 return (dest);
 }
