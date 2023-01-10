054025b2287eac639b0b843c5f549772d082a32b
Kestarich <nyavor@outlook.com>
2023-01-10 14:52:23 -0800

    1

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -8,7 +8,6 @@
  **/
 
 int count_word(char *s)
-
 {
 int flag, c, w;
 flag = 0;
@@ -31,7 +30,6 @@ return (w);
  ** Return: pointer to an array of strings (Success)
  ** or NULL (Error)
  **/
-
 char **strtow(char *str)
 {
 char **matrix, *tmp;
