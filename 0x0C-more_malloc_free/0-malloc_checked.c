9536ad6ee520f9cb8d0042315505ada9e011d981
Kestarich <nyavor@outlook.com>
2023-01-12 14:25:31 -0800

    hard

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ ** malloc_checked - Allocates memory using malloc
+ ** @b: number of bytes to be allocated
+ ** Return: void
+ **/
+
+void *malloc_checked(unsigned int b)
+
+{
+
+void *p;
+p = malloc(b);
+if (p == NULL)
+{
+exit(98);
+}
+return (p);
+}
